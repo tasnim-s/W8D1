3e16afc68fe0adcc47ff60ad4930d4f763ff1fcc
tsaiduzzaman <tasnim.s@nyu.edu>
2020-12-14 11:34:04 -0500

    Created users models and controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    before_action :ensure_logged_in, only: [:destroy]
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = ['Invalid Username and/or Password']
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+    end
+end
